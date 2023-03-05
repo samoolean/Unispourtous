6112b04af8ebe5fa37ea7493ebb9d99f0bcd169e
samoolean <61148066+samoolean@users.noreply.github.com>
2023-03-05 16:08:59 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //V.1.0.1
 
-import * as THREE from 'node_modules';
+import * as THREE from 'node_modules/three/build/three.js;
 
 const scene = new THREE.Scene();
 const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
